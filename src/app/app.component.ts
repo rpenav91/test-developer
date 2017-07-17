109979b81464b5a59593b849881b4872140d93a9
René Peña <rpenav91@hotmail.com>
2017-07-17 01:06:07 -0600

    Test Developer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import {VideosService} from './video.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers:[VideosService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'Laureates YouTube Search';
+  videos = [];
+  onsubmit(value: any){
+    console.log(value.search_value);
+  }
+  constructor(private _videosService: VideosService){}
+  ngOnInit(){
+   /*this._videosService.getVideos()
+   .subscribe(resVideosData => this.videos = resVideosData); 
+  }*/
+    this.videos = this._videosService.getVideos();
+    //console.log(this.videos);
+  }
 }
